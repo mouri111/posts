65b3ec8729a62dc119743d88034e246ff8c6c6eb
Masato Mouri <13274198+mouri111@users.noreply.github.com>
2018-08-17 18:39:31 +0900

    Add posts

--- calc-web.sh
+++ calc-web.sh
@@ -4,6 +4,11 @@ tmpfile=$(mktemp)
 
 mkdir -p _site
 
+cp -r css images _site
+
 ls {about,contact}.md | sed s/.md//g | xargs -l -iREPLACE pandoc -f markdown -t html --template=templates/default.html REPLACE.md -o _site/REPLACE.html
+
+mkdir -p _site/posts
+find posts -name "*.md" | sed s/.md//g | xargs -l -IREPLACE pandoc -f markdown -t html --template=templates/default.html REPLACE.md -o _site/REPLACE.html
 
 rm $tmpfile
